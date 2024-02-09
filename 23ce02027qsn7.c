98fcb86cba449667369cf2184a51845e20f3342f
sahi-13 <157104130+sahi-13@users.noreply.github.com>
2024-02-09 13:46:07 +0530

    Update 23ce02027qsn7.c

--- 23ce02027qsn7.c
+++ 23ce02027qsn7.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main ()
 {
-    int M,N;
-    float W,K,T;
+    int M,K,N;
+    float W,T;
     printf("Marks obtained:");
     scanf("%d",&M);
      printf("No of classes conducted:");
